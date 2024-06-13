be673cfb06ba959cf5f47c2655803e9ed6340a3a
seeww <am6891614@gmail.com>
2024-06-13 14:15:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/nornorno/mouozk-2024",
+    "https://github.com/ahmed78888/bot788",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
